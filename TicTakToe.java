61ea88b6280388eff442f6cb1d003e699bf42a7c
Himanshu <Himanshu.shivhare.9935@gmail.com>
2021-04-26 01:39:37 +0530

    function to display tictak toe board added

--- TicTakToe.java
+++ TicTakToe.java
@@ -103,8 +103,22 @@ public class TicTakToe{
 			}
 
 		}
-	
-	
+		
+		//Draw the board at the end of the game
+		drawBoard(board);
+
+  }
+        }
+
+	//Make a function to draw the tic tac toe board
+	public static void drawBoard(char[][] board) {
+		System.out.println("Board:");
+		for(int i = 0; i < 3; i++) {
+			//The inner for loop prints out each row of the board
+			for(int j = 0; j < 3; j++) {
+				System.out.print(board[i][j]);
+			}
+			//This print statement makes a new line so that each row is on a separate line
+			System.out.println();
+		}
 	}
-	
-}
