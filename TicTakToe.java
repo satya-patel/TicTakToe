c09ea121ed0ec34710ea73c03fc40d0d830af800
Himanshu <Himanshu.shivhare.9935@gmail.com>
2021-04-26 08:28:15 +0530

    full code added

--- TicTakToe.java
+++ TicTakToe.java
@@ -108,7 +108,7 @@ public class TicTakToe{
 		drawBoard(board);
 
   }
-        }
+        
 
 	//Make a function to draw the tic tac toe board
 	public static void drawBoard(char[][] board) {
@@ -122,3 +122,45 @@ public class TicTakToe{
 			System.out.println();
 		}
 	}
+	
+	public static char playerHasWon(char[][] board) {
+		
+		//Check each row
+		for(int i = 0; i < 3; i++) {
+			if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != '-') {
+				return board[i][0];
+			}
+		}
+
+		//Check each column
+		for(int j = 0; j < 3; j++) {
+			if(board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[0][j] != '-') {
+				return board[0][j];
+			}
+		}
+
+		//Check the diagonals
+		if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-') {
+			return board[0][0];
+		}
+		if(board[2][0] == board[1][1] && board[1][1] ==  board[0][2] && board[2][0] != '-') {
+			return board[2][0];
+		}
+
+		//Otherwise nobody has not won yet
+		return ' ';
+
+	}
+
+	//Make a function to check if all of the positions on the board have been filled
+	public static boolean boardIsFull(char[][] board) {
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(board[i][j] == '-') {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
